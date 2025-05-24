e143470265ba060640136b2f2ffdfa8223570a11
Luciano Madroñal <lucianoumc1@gmail.com>
2025-05-24 20:00:41 -0300

    refactor 2

--- index.js
+++ index.js
@@ -8,25 +8,25 @@ app.use(express.json());
 
 //Configuración de pug y carpeta de vistas
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "src/views"));
 
 // Determinacion del puerto del servidor
 const port = process.env.PORT || 3000;
 
 //Definicion del servidor. ruta base de prueba
 app.get("/", (req, res) => {
-	res.send("Hola Mundo");
+  res.send("Hola Mundo");
 });
 
 // Importar el archivo de rutas de turnos
-const consultaTurnosRouter = require("./routes/consultaTurnosRouter");
-const getUsuarios  = require("./routes/userRoute");
-const postUsuarios = require("./routes/userRoute");
+const consultaTurnosRouter = require("./src/routes/consultaTurnosRouter");
+const getUsuarios = require("./src/routes/userRoute");
+const postUsuarios = require("./src/routes/userRoute");
 
 app.use("/turnos", consultaTurnosRouter);
 app.use("/usuarios", getUsuarios);
 app.use("/registroUser", postUsuarios);
 
 app.listen(port, () => {
-	console.log(`Server corriendo en http://localhost:${port}`);
+  console.log(`Server corriendo en http://localhost:${port}`);
 });
