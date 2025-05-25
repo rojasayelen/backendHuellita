70b3433371d49a1c29f7500474643eedaeab7d46
Oscar J Sosa Ortiz <30639959+okijulian@users.noreply.github.com>
2025-05-25 11:19:01 -0300

    - se agrega funcionalidad y vista para delete de usuarios. (#5)
    * - se agrega funcionalidad y vista para delete de usuarios.
    
    * se vuelve el json a estado original
    
    * se corrije
    
    * se formatea los archivos
    
    * -se elimina campo tipousuario de json y de vistas

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); // <--- Aquí creas el enrutador
 const {
   getUsuarios,
   postUsuarios,
+  deleteUsuarios,
+  getAdminUsuarios,
   loginUser,
   loginForm,
 } = require("../controllers/userController");
 
-router.get("/", getUsuarios);
-router.post("/", postUsuarios);
+router.get("/", getUsuarios); // GET /usuarios
+router.get("/admin", getAdminUsuarios); // GET /usuarios/admin
+router.post("/", postUsuarios); // POST /usuarios (para crear usuario)
+router.delete("/deleteUsuario/:id", deleteUsuarios); // DELETE /usuarios/deleteUsuario/:id
 
-router.get("/login", loginForm);
-router.post("/login", loginUser);
+router.get("/login", loginForm); // GET /usuarios/login
+router.post("/login", loginUser); // POST /usuarios/login
 
-module.exports = router;
+module.exports = router; // <-- Exportas el enrutador completo
