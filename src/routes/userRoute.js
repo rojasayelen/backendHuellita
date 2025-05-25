2083bfa7a7cd11907450d2d99fef4393e599978c
Luciano Madroñal <lucianoumc1@gmail.com>
2025-05-25 13:30:14 -0300

    prev changes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const {
   getUsuarios,
   postUsuarios,
+  updateUserForm,
+  updateUsuarios,
   loginUser,
   loginForm,
 } = require("../controllers/userController");
 
 router.get("/", getUsuarios);
 router.post("/", postUsuarios);
+
+router.get("/update", updateUserForm);
+router.put("/update", updateUsuarios);
 
 router.get("/login", loginForm);
 router.post("/login", loginUser);
